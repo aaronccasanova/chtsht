e81258e2abcd739814e1df892cfe380f4d7dfb0d
Aaron Casanova <aaronccasanova@gmail.com>
2022-02-13 12:24:44 -0800

    fix: Missing ui.js import

--- source/cli.tsx
+++ source/cli.tsx
@@ -1,36 +1,36 @@
 #!/usr/bin/env node
-import React from 'react';
-import {render} from 'ink';
-import meow from 'meow';
-import App from './ui';
+import React from "react";
+import { render } from "ink";
+import meow from "meow";
+import App from "./ui";
 
 const cli = meow(
 	`
 	Usage
-	  $ npx @aacc/cheatsheet <name>
+	  $ npx chtsht <name>
 
 	Options
 	  --exec, -e  Execute the command
 
 	Examples
 	  # Show select list of all cheat sheets
-	  $ npx @aacc/cheatsheet
+	  $ npx chtsht
 
 	  # Show the docker cheat sheet
-	  $ npx @aacc/cheatsheet docker
+	  $ npx chtsht docker
 
 		# Execute the command
-		$ npx @aacc/cheatsheet -e docker
+		$ npx chtsht -e docker
 `,
 	{
 		flags: {
 			exec: {
-				alias: 'e',
-				type: 'boolean',
-				default: false
-			}
-		}
+				alias: "e",
+				type: "boolean",
+				default: false,
+			},
+		},
 	}
 );
 
-render(<App cheatsheet={cli.input[0]} exec={cli.flags.exec}/>);
+render(<App cheatsheet={cli.input[0]} exec={cli.flags.exec} />);
